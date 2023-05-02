70785ab841d66101726f8e5c6903b07b91225d32
Henrique S. Xavier <hxavier@nic.br>
2023-05-01 23:16:08 -0300

    live-monitor tutorial: web conf demo

---
+++ tutoriais/live-monitor/git_push_analysis.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Enter upload script..."
+stamp=`date +%Y-%m-%d_%H:%M:%S`
+msg="Auto-update webpage data >> $stamp"
+
+echo "Add updates..."
+git add ../../assets
+
+echo "Commit updates..."
+git commit -m "$msg"
+
+echo "Push updates..."
+git push
