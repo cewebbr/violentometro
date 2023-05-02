33c74c37db0d83b4e8822012be6e501ba4718ef0
Henrique S. Xavier <hxavier@nic.br>
2023-05-02 18:00:07 -0300

    Auto-update webpage data >> 2023-05-02_18:00:07

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,1 +1,1 @@
-data = '{"total_counts": 6643, "yesterday_counts": 1752, "time_series_plot": "../../assets/img/graficos/tweets_agressivos_por_dia_feminino.png", "last_update": "2023-05-02T17:00:07"}'
+data = '{"total_counts": 6761, "yesterday_counts": 1752, "time_series_plot": "../../assets/img/graficos/tweets_agressivos_por_dia_feminino.png", "last_update": "2023-05-02T18:00:07"}'
